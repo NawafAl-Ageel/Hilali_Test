093c1ffed7acb7120ed6d7906fb9c61ff899adca
Nawaf Alageel <146536903+NawafAl-Ageel@users.noreply.github.com>
2025-10-27 23:27:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const challengeCategories = [
           type: "image",
           label: "من هو هذا اللاعب؟",
           required: true,
-          image: "images/q1/malcom_covered.jpg",
+          image: "images/q1/Malcom_Uncovered.jpg.jpg",
           options: [
             {value:"malcolm", label:"مالكوم"},
             {value:"neymar", label:"نيمار"},
